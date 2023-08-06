c032ee417a4329db40343282e1a56dd8ae8e410e
jun14111 <1411187249@qq.com>
2023-08-07 00:45:41 +0800

    temp

---
+++ quant-domain/src/main/java/org/huangcj/quant/domain/gateway/Gateway.java
@@ -0,0 +1,8 @@
+package org.huangcj.quant.domain.gateway;
+
+/**
+ * @author HuangChengJun
+ * @date 2023/8/7 00:39
+ */
+public interface Gateway {
+}
---
+++ quant-domain/src/main/java/org/huangcj/quant/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This is the core Domain business which should be pure and clean, have no dependency over other layers.
+ *
+ * @author HuangChengJun
+ * @date 2023/8/7 00:40
+ */
+package org.huangcj.quant.domain;
